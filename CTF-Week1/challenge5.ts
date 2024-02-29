3d0697dd33a30adc9f76f33e7e3846d00a22f344
Nandom Alfred <nandommamdam@gmail.com>
2024-02-29 05:31:39 +0100

    done with challenge5

--- CTF-Week1/challenge5.ts
+++ CTF-Week1/challenge5.ts
@@ -1,67 +1,92 @@
-import { Connection, Keypair, SystemProgram, PublicKey } from "@solana/web3.js"
-import { Program, Wallet, AnchorProvider, Address, BN } from "@project-serum/anchor"
-import { Week1, IDL } from "./programs/week1";
-import { ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID, getOrCreateAssociatedTokenAccount, mintTo } from "@solana/spl-token";
+import { Connection, Keypair, SystemProgram, PublicKey } from '@solana/web3.js'
+import {
+	Program,
+	Wallet,
+	AnchorProvider,
+	Address,
+	BN,
+} from '@project-serum/anchor'
+import { Week1, IDL } from './programs/week1'
+import {
+	ASSOCIATED_TOKEN_PROGRAM_ID,
+	TOKEN_PROGRAM_ID,
+	getOrCreateAssociatedTokenAccount,
+	mintTo,
+} from '@solana/spl-token'
 
-import wallet from "./wallet/wba-wallet.json"
+import wallet from './wallet/wba-wallet.json'
 
 // We're going to import our keypair from the wallet file
-const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+const keypair = Keypair.fromSecretKey(new Uint8Array(wallet))
 
 // Create a devnet connection
-const connection = new Connection("https://api.devnet.solana.com");
+const connection = new Connection('https://api.devnet.solana.com')
 
 // Create our anchor provider
-const provider = new AnchorProvider(connection, new Wallet(keypair), { commitment: "confirmed"});
+const provider = new AnchorProvider(connection, new Wallet(keypair), {
+	commitment: 'confirmed',
+})
 
 // Create our program
-const program = new Program<Week1>(IDL, "ctf1VWeMtgxa24zZevsXqDg6xvcMVy4FbP3cxLCpGha" as Address, provider);
+const program = new Program<Week1>(
+	IDL,
+	'ctf1VWeMtgxa24zZevsXqDg6xvcMVy4FbP3cxLCpGha' as Address,
+	provider
+)
 
 // Use the PDA for our CTF-Week1 profile
-const profilePda = PublicKey.findProgramAddressSync([Buffer.from("profile"), keypair.publicKey.toBuffer()], program.programId)[0];
+const profilePda = PublicKey.findProgramAddressSync(
+	[Buffer.from('profile'), keypair.publicKey.toBuffer()],
+	program.programId
+)[0]
 
 // Use the PDA for the Auth account
-// const authPda = ???
+const authPda = PublicKey.findProgramAddressSync(
+	[Buffer.from('auth')],
+	program.programId
+)[0]
 
 // Paste here the mint address for challenge1 token
-const mint = new PublicKey("6SFccXYgSc7yB7id92PTMoVXkGcXGcpMJD74GgZKbfQ4");
+const mint = new PublicKey('5YwWffGZ7JW9z6yUuePPV9JciNjGVvmhmoYmxVtfu2uu')
 
-(async () => {
-    try {
-
-        // Create the ATA for your Wallet
-        // const ownerAta = getOrCreateAssociatedTokenAccount(
-        //     ???
-        // );
-
-        // Mint some tokens!
-        // const mintTx = await mintTo(
-        //     ???
-        //     (await ownerAta).address, 
-        //     ???
-        // ) 
-        
-        // Complete the Challenge!
-        // const completeTx = await program.methods
-        // .completeChallenge5()
-        // .accounts({
-        //     owner: 
-        //     ata: (await ownerAta).address,
-        //     profile: 
-        //     mint: 
-        //     authority: 
-        //     tokenProgram: 
-        //     associatedTokenProgram: 
-        //     systemProgram:
-        // })
-        // .signers([
-        //     keypair
-        // ]).rpc();
-
-        // console.log(`Success! Check out your TX here: 
-        // https://explorer.solana.com/tx/${completeTx}?cluster=devnet`);
-
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
-})();
+;(async () => {
+	try {
+		// Create the ATA for your Wallet
+		const ownerAta = getOrCreateAssociatedTokenAccount(
+			connection,
+			keypair,
+			mint,
+			keypair.publicKey
+		)
+		// Mint some tokens!
+		const mintTx = await mintTo(
+			connection,
+			keypair,
+			mint,
+			(
+				await ownerAta
+			).address,
+			keypair.publicKey,
+			10000
+		)
+		// Complete the Challenge!
+		const completeTx = await program.methods
+			.completeChallenge5()
+			.accounts({
+				owner: keypair.publicKey,
+				ata: (await ownerAta).address,
+				profile: profilePda,
+				mint,
+				authority: authPda,
+				tokenProgram: TOKEN_PROGRAM_ID,
+				associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+				systemProgram: SystemProgram.programId,
+			})
+			.signers([keypair])
+			.rpc()
+		console.log(`Success! Check out your TX here:
+		https://explorer.solana.com/tx/${completeTx}?cluster=devnet`)
+	} catch (e) {
+		console.error(`Oops, something went wrong: ${e}`)
+	}
+})()
