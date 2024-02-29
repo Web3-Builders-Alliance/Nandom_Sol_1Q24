a241cf5d1be9e4846b01df8847b0959c58bd0a38
Nandom Alfred <nandommamdam@gmail.com>
2024-02-29 05:03:23 +0100

    done with challenge4

--- CTF-Week1/challenge4.ts
+++ CTF-Week1/challenge4.ts
@@ -1,78 +1,115 @@
-import { Connection, Keypair, SystemProgram, PublicKey, LAMPORTS_PER_SOL } from "@solana/web3.js"
-import { Program, Wallet, AnchorProvider, Address, BN } from "@project-serum/anchor"
-import { Week1, IDL } from "./programs/week1";
-import { ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID, getOrCreateAssociatedTokenAccount, mintTo } from "@solana/spl-token";
-import { 
-    MPL_TOKEN_METADATA_PROGRAM_ID as TOKEN_METADATA_PROGRAM_ID
-  } from '@metaplex-foundation/mpl-token-metadata';
+import {
+	Connection,
+	Keypair,
+	SystemProgram,
+	PublicKey,
+	LAMPORTS_PER_SOL,
+} from '@solana/web3.js'
+import {
+	Program,
+	Wallet,
+	AnchorProvider,
+	Address,
+	BN,
+} from '@project-serum/anchor'
+import { Week1, IDL } from './programs/week1'
+import {
+	ASSOCIATED_TOKEN_PROGRAM_ID,
+	TOKEN_PROGRAM_ID,
+	getOrCreateAssociatedTokenAccount,
+	mintTo,
+} from '@solana/spl-token'
+import { MPL_TOKEN_METADATA_PROGRAM_ID as TOKEN_METADATA_PROGRAM_ID } from '@metaplex-foundation/mpl-token-metadata'
 
-import wallet from "./wallet/wba-wallet.json"
+import wallet from './wallet/wba-wallet.json'
 
 // We're going to import our keypair from the wallet file
-const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+const keypair = Keypair.fromSecretKey(new Uint8Array(wallet))
 
 // Create a devnet connection
-const connection = new Connection("https://api.devnet.solana.com");
+const connection = new Connection('https://api.devnet.solana.com')
 
 // Create our anchor provider
-const provider = new AnchorProvider(connection, new Wallet(keypair), { commitment: "confirmed"});
+const provider = new AnchorProvider(connection, new Wallet(keypair), {
+	commitment: 'confirmed',
+})
 
 // Create our program
-const program = new Program<Week1>(IDL, "ctf1VWeMtgxa24zZevsXqDg6xvcMVy4FbP3cxLCpGha" as Address, provider);
+const program = new Program<Week1>(
+	IDL,
+	'ctf1VWeMtgxa24zZevsXqDg6xvcMVy4FbP3cxLCpGha' as Address,
+	provider
+)
 
 // Use the PDA for our CTF-Week1 profile
-const profilePda = PublicKey.findProgramAddressSync([Buffer.from("profile"), keypair.publicKey.toBuffer()], program.programId)[0];
+const profilePda = PublicKey.findProgramAddressSync(
+	[Buffer.from('profile'), keypair.publicKey.toBuffer()],
+	program.programId
+)[0]
 
 // Paste here the mint address for challenge1 token
-const mint = new PublicKey("7u7ds4TpkKNAnGeoUTTwbGsJg2wBWTHR7jqK2d2UBD7u");
+// const mint = new PublicKey('Dzuy1EUmKyEjvfrKq9Jnksv4UEgVQ3qqRto1dfzRa3e4')
+const mint = new PublicKey('EUeBcjRdZL2uuwSHB6zhvJZBUvbmZ2CFmwX8YUTCqe4M')
 
 // Create the PDA for the Challenge1 Vault
-const vault = PublicKey.findProgramAddressSync([Buffer.from("vault4"), keypair.publicKey.toBuffer(), mint.toBuffer()], program.programId)[0];
+const vault = PublicKey.findProgramAddressSync(
+	[Buffer.from('vault4'), keypair.publicKey.toBuffer(), mint.toBuffer()],
+	program.programId
+)[0]
 
-const metadata_program = new PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s");
+const metadata_program = new PublicKey(
+	'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s'
+)
 
 // Create PDA for token metadata
 const metadata_seeds = [
-    Buffer.from('metadata'),
-    metadata_program.toBuffer(),
-    mint.toBuffer(),
-];
-const metadata = PublicKey.findProgramAddressSync(metadata_seeds, metadata_program)[0];
+	Buffer.from('metadata'),
+	metadata_program.toBuffer(),
+	mint.toBuffer(),
+]
+const metadata = PublicKey.findProgramAddressSync(
+	metadata_seeds,
+	metadata_program
+)[0]
 
-(async () => {
+;(async () => {
+	// NB if you get TokenAccountNotFoundError, wait a few seconds and try again!
+	// Create the ATA for your Wallet
+	const ownerAta = getOrCreateAssociatedTokenAccount(
+		connection,
+		keypair,
+		mint,
+		keypair.publicKey
+	)
+	// Mint some tokens!
+	const mintTx = await mintTo(
+		connection,
+		keypair,
+		mint,
+		(
+			await ownerAta
+		).address,
+		keypair.publicKey,
+		LAMPORTS_PER_SOL
+	)
 
-        // NB if you get TokenAccountNotFoundError, wait a few seconds and try again!
-
-        // Create the ATA for your Wallet
-        // const ownerAta = getOrCreateAssociatedTokenAccount(
-        //     ???
-        // );
-
-        // Mint some tokens!
-        // const mintTx = await mintTo(
-        //     ???
-        //     (await ownerAta).address, 
-        //     ???
-        // ) 
-        
-        // Complete the Challenge!
-        // const completeTx = await program.methods.completeChallenge4()
-        // .accounts({
-        //     owner:
-        //     ata: (await ownerAta).address,
-        //     profile: 
-        //     vault: 
-        //     metadata: 
-        //     mint: 
-        //     tokenProgram:
-        //     metadataProgram: 
-        //     associatedTokenProgram: 
-        //     systemProgram: 
-        // })
-        // .signers([
-        //     keypair
-        // ]).rpc();
-
-        // console.log(`Success! Check out your TX here: 
-        // https://explorer.solana.com/tx/${completeTx}?cluster=devnet`);
-})();
+	// Complete the Challenge!
+	const completeTx = await program.methods
+		.completeChallenge4()
+		.accounts({
+			owner: keypair.publicKey,
+			ata: (await ownerAta).address,
+			profile: profilePda,
+			vault: vault,
+			metadata: metadata,
+			mint: mint,
+			tokenProgram: TOKEN_PROGRAM_ID,
+			metadataProgram: TOKEN_METADATA_PROGRAM_ID,
+			associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+			systemProgram: SystemProgram.programId,
+		})
+		.signers([keypair])
+		.rpc()
+	console.log(`Success! Check out your TX here:
+  https://explorer.solana.com/tx/${completeTx}?cluster=devnet`)
+})()
--- CTF-Week1/createMetadata4.ts
+++ CTF-Week1/createMetadata4.ts
@@ -1,43 +1,85 @@
-import { Commitment, Connection, Keypair, Transaction, sendAndConfirmTransaction, PublicKey, Signer } from "@solana/web3.js"
-import wallet from "./wallet/wba-wallet.json"
-import {createMetadataAccountV3 } from "@metaplex-foundation/mpl-token-metadata";
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { publicKey, signerIdentity, createSignerFromKeypair } from '@metaplex-foundation/umi';
-import { publicKey as publicKeySerializer, string } from '@metaplex-foundation/umi/serializers';
-import { base58 } from "@metaplex-foundation/umi/serializers";
-
+import { Commitment, Connection } from '@solana/web3.js'
+import wallet from './wallet/wba-wallet.json'
+import {
+	CreateMetadataAccountV3InstructionAccounts,
+	CreateMetadataAccountV3InstructionDataArgs,
+	DataV2Args,
+	createMetadataAccountV3,
+} from '@metaplex-foundation/mpl-token-metadata'
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults'
+import {
+	publicKey,
+	signerIdentity,
+	createSignerFromKeypair,
+} from '@metaplex-foundation/umi'
+import {
+	publicKey as publicKeySerializer,
+	string,
+} from '@metaplex-foundation/umi/serializers'
+import { base58 } from '@metaplex-foundation/umi/serializers'
 
 //Create a Solana devnet connection
-const commitment: Commitment = "confirmed";
-const connection = new Connection("https://api.devnet.solana.com", commitment);
+const commitment: Commitment = 'confirmed'
+const connection = new Connection('https://api.devnet.solana.com', commitment)
 
 //Create a Umi instance
-const RPC_ENDPOINT = "https://api.devnet.solana.com";
-const umi = createUmi(RPC_ENDPOINT);
+const RPC_ENDPOINT = 'https://api.devnet.solana.com'
+const umi = createUmi(RPC_ENDPOINT)
 
-let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
-const signerKeypair = createSignerFromKeypair(umi, keypair);
-umi.use(signerIdentity(signerKeypair));
+let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet))
+const signerKeypair = createSignerFromKeypair(umi, keypair)
+umi.use(signerIdentity(signerKeypair))
 
-const mint =  publicKey('7u7ds4TpkKNAnGeoUTTwbGsJg2wBWTHR7jqK2d2UBD7u')
-const tokenMetadataProgramId = publicKey('metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s');
+const mint = publicKey('EUeBcjRdZL2uuwSHB6zhvJZBUvbmZ2CFmwX8YUTCqe4M')
+const tokenMetadataProgramId = publicKey(
+	'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s'
+)
 
-const seeds = 
-  [string({ size: 'variable' }).serialize('metadata'),
-  publicKeySerializer().serialize(tokenMetadataProgramId),
-  publicKeySerializer().serialize(mint),
-];
-const metadata_pda = umi.eddsa.findPda(tokenMetadataProgramId, seeds);
+const seeds = [
+	string({ size: 'variable' }).serialize('metadata'),
+	publicKeySerializer().serialize(tokenMetadataProgramId),
+	publicKeySerializer().serialize(mint),
+]
+const metadata_pda = umi.eddsa.findPda(tokenMetadataProgramId, seeds)
 
-(async () => {
+;(async () => {
+	let accounts: CreateMetadataAccountV3InstructionAccounts = {
+		metadata: metadata_pda,
+		mint: mint, // PublicKey | Pda;
+		mintAuthority: signerKeypair, // Signer
+	}
 
-    // let tx = createMetadataAccountV3(
-    //     ???
-    // );
+	let data: DataV2Args = {
+		name: 'WBA',
+		symbol: 'WBA',
+		uri: '',
+		sellerFeeBasisPoints: 30,
+		creators: [
+			{
+				address: keypair.publicKey,
+				verified: true,
+				share: 100,
+			},
+		],
+		collection: null,
+		uses: null,
+	}
 
-    // let result = await tx.sendAndConfirm(umi);
-    // const signature = base58.deserialize(result.signature);
+	let args: CreateMetadataAccountV3InstructionDataArgs = {
+		data,
+		isMutable: true,
+		collectionDetails: null,
+	}
 
-    // console.log(`Succesfully Minted!. Transaction Here: https://solana.fm/tx/${signature[0]}?cluster=devnet`)
-    
-})();
+	let tx = createMetadataAccountV3(umi, {
+		...accounts,
+		...args,
+	})
+
+	let result = await tx.sendAndConfirm(umi)
+	const signature = base58.deserialize(result.signature)
+
+	console.log(
+		`Succesfully Minted!. Transaction Here: https://solana.fm/tx/${signature[0]}?cluster=devnet`
+	)
+})()
